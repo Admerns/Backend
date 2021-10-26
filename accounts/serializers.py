38b16803fb07dc99a06f4b6191a328d3365dcdb0
Ali Rezakhoo <ali.rko1379@gmail.com>
2021-10-26 20:46:26 +0330

    add fname and lname

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -11,10 +11,14 @@ class UserSerializer(serializers.ModelSerializer):
 class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'username', 'email', 'password')
-        extra_kwargs = {'password': {'write_only': True}}
+        fields = ('id', 'username', 'email', 'password', 'first_name', 'last_name')
+        extra_kwargs = {'password': {'write_only': True},'first_name': {'required': True},'last_name': {'required': True}}
 
     def create(self, validated_data):
         user = User.objects.create_user(validated_data['username'], validated_data['email'], validated_data['password'])
+
+        user.first_name = validated_data['first_name']
+        user.last_name  = validated_data['last_name']
+        user.save()
 
         return user
