5bcadae6101e71fa6ecbfe2954f10e6edf2327c5
mpd00 <parali68@gmail.com>
2021-11-02 23:14:01 +0330

    Created task Model and tasks table in DB

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class task(models.Model):
+    userid = models.IntegerField(blank=False)
+    title = models.CharField(max_length=500, blank=False)
+    time = models.DateTimeField(auto_now_add=True)
+    category = models.TextField()
+    description = models.TextField()
+    alarm_check = models.TextField()
+    push_notification = models.TextField()
+
+    class Meta:
+        db_table = 'tasks'
