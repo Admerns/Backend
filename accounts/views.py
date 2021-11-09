174182bfa5643557cf2fae67069a537a32cbac85
ali-rko <Ali.rko1379@gmail.com>
2021-11-09 20:13:34 +0330

    GetTaskAPI

--- accounts/views.py
+++ accounts/views.py
@@ -28,6 +28,8 @@ class RegisterAPI(generics.GenericAPIView):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         user = serializer.save()
+        profile = UserProfile(user=user)
+        profile.save()
         return Response({
         "user": UserSerializer(user, context=self.get_serializer_context()).data
         })
