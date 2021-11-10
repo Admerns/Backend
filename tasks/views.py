014f3b306af54f68c1028a6bc0ef1a2870c26efb
ali-rko <Ali.rko1379@gmail.com>
2021-11-10 17:55:39 +0330

    EditTaskAPI

--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,7 @@
-from rest_framework import generics
+from rest_framework import generics, status
 
 from tasks.models import task
-from .serializers import Task_CreateSerializer, Task_GetSerializer
+from .serializers import Task_CreateSerializer, Task_GetSerializer, Task_EditSerializer
 from rest_framework.response import Response
 
 from rest_framework.permissions import IsAuthenticated 
@@ -33,6 +33,57 @@ class GetTasksAPI(generics.GenericAPIView):
         serializer = (self.get_serializer(tasks, many=True))
         
         return Response(serializer.data)
+
+class EditTasksAPI(generics.UpdateAPIView):
+    serializer_class = Task_EditSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def update(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+
+
+        if serializer.is_valid():
+            if ('task_token' in serializer.data):
+                editingTask = task.objects.filter(userid = request.user.id , task_token = serializer.data['task_token']).first()
+            else:
+                response = {
+                    'message': 'task_token is required.',
+                }
+                return Response(response)
+
+            if (editingTask == None):
+                response = {
+                    'message': 'Task not found.',
+                }
+                return Response(response)
+
+            if(serializer.data.get("title") != None):
+                editingTask.title = (serializer.data.get("title"))
+            if(serializer.data.get("description") != None):
+                editingTask.description = (serializer.data.get("description"))
+            if(serializer.data.get("time") != None ):
+                editingTask.time = (serializer.data.get("time"))
+            if(serializer.data.get("category") != None ):
+                editingTask.category = (serializer.data.get("category"))
+            if(serializer.data.get("alarm_check") != None ):
+                editingTask.alarm_check = (serializer.data.get("alarm_check"))
+            if(serializer.data.get("push_notification") != None ):
+                editingTask.push_notification = (serializer.data.get("push_notification"))
+
+                
+            editingTask.save()
+
+
+            response = {
+                'status': 'success',
+                'code': status.HTTP_200_OK,
+                'message': 'Task updated successfully',
+                'data': []
+            }
+
+            return Response(response)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 
