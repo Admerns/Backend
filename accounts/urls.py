323686c0c5f5c12d126c45627b3253ca6706f4c3
ali-rko <Ali.rko1379@gmail.com>
2021-11-17 02:14:49 +0330

    add static url to see avatars

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,6 +2,9 @@ from .views import RegisterAPI, LoginAPI, ChangePasswordView, EditAPI, CurrentUs
 from django.urls import path, include
 from knox import views as knox_views
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 
 
@@ -14,4 +17,4 @@ urlpatterns = [
     path('logout/', knox_views.LogoutView.as_view(), name='logout'),
     path('change-password/', ChangePasswordView.as_view(), name='change-password'),
     path('password-reset/', include('django_rest_passwordreset.urls', namespace='password-reset')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
