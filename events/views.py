312cd0975b19c6c2277e3c4e7ff61e565a6db29a
ali-rko <Ali.rko1379@gmail.com>
2021-12-02 15:54:41 +0330

    GetEventsAPI

--- events/views.py
+++ events/views.py
@@ -1,9 +1,11 @@
+from django.db.models.query import QuerySet
 from django.shortcuts import render
-from .serializers import Event_CreateSerializer, SessionSerializer
+from .serializers import Event_CreateSerializer, Event_GetSerializer, SessionSerializer, Event_EnterSerializer
 from rest_framework import generics, status
 from rest_framework.fields import empty
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated 
+from .models import event
 
 # Create your views here.
 class EventsAPI(generics.GenericAPIView):
@@ -14,28 +16,48 @@ class EventsAPI(generics.GenericAPIView):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         event = serializer.save()
-        return Response(request.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-        #return Sessions(request, event)
-def Sessions(request, event):
-    
-    for session in request.data.getlist('sessions'):
-        print(session)
-        data={'time':'sad','limit':'12','event_id':event.id}
-        print(data)
-        serializer = SessionSerializer(data = data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        response = {
-                    'status': 'success',
-                    'code': status.HTTP_200_OK,
-                    'message': 'Password updated successfully',
-                    'data': []
-                }
-
-        return Response(response)
-
-class SessionsAPI(generics.GenericAPIView):
-    serializer_class = SessionSerializer
+class EnterEventAPI(generics.GenericAPIView):
+    serializer_class = Event_EnterSerializer
+    permission_classes = (IsAuthenticated,)
     def post(self, request, *args, **kwargs):
-        print("ll")
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        e = event.objects.filter(event_token = serializer.data['event_token']).first()
+
+        event = serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+class GetEventsAPI(generics.GenericAPIView):
+    serializer_class = Event_GetSerializer
+    def get(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data = request.data)
+        serializer.is_valid(raise_exception=True)
+
+        events = event.objects.all()
+        
+        serializer = (self.get_serializer(events, many=True))
+        return Response(serializer.data)
+# def Sessions(request, event):
+    
+#     for session in request.data.getlist('sessions'):
+#         print(session)
+#         data={'time':'sad','limit':'12','event_id':event.id}
+#         print(data)
+#         serializer = SessionSerializer(data = data)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+#         response = {
+#                     'status': 'success',
+#                     'code': status.HTTP_200_OK,
+#                     'message': 'Password updated successfully',
+#                     'data': []
+#                 }
+
+#         return Response(response)
+
+# class SessionsAPI(generics.GenericAPIView):
+#     serializer_class = SessionSerializer
+#     def post(self, request, *args, **kwargs):
+#         print("ll")
