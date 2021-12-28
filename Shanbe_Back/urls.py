c72166115a0634cd2238190e4984abf627d13f85
mpd00 <parali68@gmail.com>
2021-12-28 21:20:23 +0330

    Change urls and categorize them by functionality

--- Shanbe_Back/urls.py
+++ Shanbe_Back/urls.py
@@ -20,8 +20,8 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('admin/', admin.site.urls),
-    path('', include('accounts.urls')),
-    path('', include('tasks.urls')),
-    path('', include('events.urls')),
-    path('', include('googlecalendar.urls')),
+    path('accounts/', include('accounts.urls')),
+    path('tasks/', include('tasks.urls')),
+    path('events/', include('events.urls')),
+    path('calendar/', include('googlecalendar.urls')),
 ] 
