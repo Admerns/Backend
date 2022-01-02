81b80c0748a232f9eec39c7fffcf095fd0c38766
mpd00 <parali68@gmail.com>
2022-01-02 22:59:14 +0330

    Create coordinates API and serializers to find city by coordinates

--- googlecalendar/views.py
+++ googlecalendar/views.py
@@ -3,7 +3,10 @@ from django.shortcuts import render
 from rest_framework.permissions import IsAuthenticated 
 from rest_framework.response import Response
 from rest_framework import generics, status
-from .serializers import GoogleCalendarLogin_CreateSerializer
+from .serializers import GoogleCalendarLogin_CreateSerializer, Coordinates_CreateSerializer
+from accounts.models import Metadata
+from geopy.geocoders import Nominatim
+
 # Create your views here.
 
 class GoogleCalendarLoginAPI(generics.GenericAPIView):
@@ -17,3 +20,47 @@ class GoogleCalendarLoginAPI(generics.GenericAPIView):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+class CoordinatesAPI(generics.GenericAPIView):
+    serializer_class = Coordinates_CreateSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def post(self, request, *args, **kwargs):
+
+        user = request.user
+        try :
+            metadata = user.metadata
+  
+        except Exception as e:
+            metadata = Metadata(user=user)
+
+
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        try:
+            geolocator = Nominatim(user_agent="geoapiExercises")
+            Latitude = request.data['latitude']
+            Longitude = request.data['longitude']
+            location = geolocator.reverse(Latitude+","+Longitude, language='en')
+            
+            try:
+                city = str(location.raw['address']['state'])
+                metadata.city = city
+            except:
+                city = str(location.raw['address']['province'])
+                metadata.city = city
+
+            #metadata.city = city
+            metadata.save()
+            response = {
+                        'status': 'success',
+                        'code': status.HTTP_200_OK,
+                        'message': 'City added successfuly.',
+                        'data': []
+                    }
+        except:
+            response = {
+                'message': 'Session not found.',
+            }
+
+        return Response(response)
